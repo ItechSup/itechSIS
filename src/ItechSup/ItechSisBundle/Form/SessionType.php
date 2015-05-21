0d5c27ea83cab7ede83feb1bf7fd3a45e3094c88
Dinaam <dinaam.n@gmail.com>
2015-05-21 19:31:14 +0200

    Session/Student crud// Error.

---
+++ src/ItechSup/ItechSisBundle/Form/SessionType.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace ItechSup\ItechSisBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class SessionType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('startDate')
+            ->add('endDate')
+            ->add('formation')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'ItechSup\ItechSisBundle\Entity\Session'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'itechsup_itechsisbundle_session';
+    }
+}
