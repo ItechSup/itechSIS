3462eb1fe3699e747e8b792285f025713ae094c6
katchou <jean.francois.jung@gmail.com>
2015-05-26 22:28:07 +0200

    some more fixes

--- src/ItechSup/ItechSisBundle/Controller/DefaultController.php
+++ src/ItechSup/ItechSisBundle/Controller/DefaultController.php
@@ -4,6 +4,7 @@ namespace ItechSup\ItechSisBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
 class DefaultController extends Controller
