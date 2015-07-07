097bafc6922f72f297358ac7ef9afd898cd46242
katchou <jean.francois.jung@gmail.com>
2015-07-07 18:11:12 +0200

    done with Room, moving to Formation

--- src/ItechSup/ItechSisBundle/Controller/RoomController.php
+++ src/ItechSup/ItechSisBundle/Controller/RoomController.php
@@ -240,7 +240,7 @@ class RoomController extends Controller
         return $this->createFormBuilder()
             ->setAction($this->generateUrl('room_delete', array('id' => $id)))
             ->setMethod('DELETE')
-            ->add('submit', 'submit', array('label' => 'Delete'))
+            ->add('submit', 'submit', array('label' => 'Supprimer'))
             ->getForm()
         ;
     }
---
+++ src/ItechSup/ItechSisBundle/Menu/Builder.php
@@ -0,0 +1,50 @@
+<?php
+namespace ItechSup\ItechSisBundle\Menu;
+
+use Knp\Menu\FactoryInterface;
+use Symfony\Component\DependencyInjection\ContainerAware;
+
+class Builder extends ContainerAware
+{
+    public function mainMenu(FactoryInterface $factory, array $options)
+    {
+        $menu = $factory->createItem('root');
+        $menu->setChildrenAttribute('class', 'nav navbar-nav');
+
+        /**
+         * Eleves
+         */
+        $menu->addChild('Elèves', array('route' => 'student'))
+            ->setAttribute('icon', 'fa fa-list');
+        
+        /**
+         * Enseignants
+         */
+        $menu->addChild('Enseignants', array('route' => 'teacher'))
+            ->setAttribute('icon', 'fa fa-list');
+        
+        /**
+         * Classes
+         */
+        $menu->addChild('Classes', array('route' => 'formation'))
+            ->setAttribute('icon', 'fa fa-list');
+        
+        /**
+         * Configuration
+         */
+        $menu->addChild('Configuration')
+            ->setAttribute('icon', 'fa fa-list')
+            ->setAttribute('dropdown', true);
+        
+        $menu['Configuration']->addChild('Salles', array('route' => 'room'))
+            ->setAttribute('icon', 'fa fa-list');
+        
+        $menu['Configuration']->addChild('Cursus', array('route' => 'formation'))
+            ->setAttribute('icon', 'fa fa-list');
+        
+        $menu['Configuration']->addChild('Ecoles', array('route' => 'school'))
+            ->setAttribute('icon', 'fa fa-list');
+
+        return $menu;
+    }
+}
